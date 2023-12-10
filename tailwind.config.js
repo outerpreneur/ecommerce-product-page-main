f0e05a417f1de5a109435f9aaf249d67e0a08486
Alek Tysklind <91225228+outerpreneur@users.noreply.github.com>
2023-12-10 23:49:32 +0100

    styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        fontKumbh: "Kumbh Sans",
+      },
+      colors: {
+        accentOrange: "hsl(26, 100%, 55%)",
+        paleOrange: "hsl(25, 100%, 94%)",
+        veryDarkBlue: "hsl(220, 13%, 13%)",
+        darkGrayishBlue: "hsl(219, 9%, 45%)",
+        grayishBlue: "hsl(220, 14%, 75%)",
+        lightGrayishBlue: "hsl(223, 64%, 98%)",
+        specialWhite: "hsl(0, 0%, 100%)",
+        specialBlack: "hsl(0, 0%, 0%)",
+      },
+      backgroundColor: {
+        accentOrange: "hsl(26, 100%, 55%)",
+        paleOrange: "hsl(25, 100%, 94%)",
+        veryDarkBlue: "hsl(220, 13%, 13%)",
+        darkGrayishBlue: "hsl(219, 9%, 45%)",
+        grayishBlue: "hsl(220, 14%, 75%)",
+        lightGrayishBlue: "hsl(223, 64%, 98%)",
+        specialWhite: "hsl(0, 0%, 100%)",
+        specialBlack: "hsl(0, 0%, 0%)",
+      },
+    },
   },
   plugins: [],
 };
